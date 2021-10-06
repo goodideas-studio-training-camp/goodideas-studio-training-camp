4cbe68c97740a0760fffac3d5e71bb9c6d67c27e
G100my <G100my.lan229@gmail.com>
2021-10-07 02:58:39 +0800

    feat: add top nav link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,34 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'one',
+        items: [
+          { text: 'one - 文章 1', link: '/one/fake1' },
+          {
+            text: 'one - 分類',
+            items: [
+              { text: 'one - 文章 2', link: '/one/fake2' },
+              { text: 'one - 文章 3', link: '/one/fake3' },
+            ],
+          },
+        ],
+      },
+      {
+        text: 'two',
+        items: [
+          { text: 'two - 文章 1', link: '/one' },
+          { text: 'two - 文章 2', link: '/two' },
+        ],
+      },
+      {
+        text: 'wrong path',
+        link: '/wrongPath',
+      },
+      { text: 'G100 vuepress blog', link: 'https://g100my.github.io/G100-blog/' },
+    ],
+  },
 }
