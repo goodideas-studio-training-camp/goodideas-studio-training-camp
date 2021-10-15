5b3dc42223e2532d8fd4673081b79d4031ef1ef5
G100 <G100my.lan229@gmail.com>
2021-10-15 15:47:35 +0800

    init sidebar, nav, fake article

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+const { makeNavRoute, makeSidebarRoute } = require('../../utils/routeMaker')
+
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    navbar: [
+      { text: 'Home', link: '/' },
+      //   {
+      //     text: 'one',
+      //     children: [
+      //       { text: 'one - 文章 1', link: '/one/01-fake1' },
+      //       {
+      //         text: 'one - 分類',
+      //         children: [
+      //           { text: 'one - 文章 2', link: '/one/02-fake2' },
+      //           { text: 'one - 文章 3', link: '/one/03-fake3' },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     text: 'two',
+      //     children: [
+      //       { text: 'two - 文章 1', link: '/two/01-one' },
+      //       { text: 'two - 文章 2', link: '/two/02-two' },
+      //     ],
+      //   },
+      ...makeNavRoute('one'),
+      ...makeNavRoute('two'),
+      {
+        text: 'wrong path',
+        link: '/wrongPath',
+      },
+      { text: 'G100 vuepress blog', link: 'https://g100my.github.io/G100-blog/' },
+    ],
+    sidebar: { ...makeSidebarRoute('one') },
+  },
+}
---
+++ docs/one/01-fake1.md
@@ -0,0 +1,16 @@
+---
+title: 假文章 one
+lang: zh-TW
+description: YOYOYO
+# layout: 設定這個頁面所要使用的 layout template component
+# permalink: 永久連結
+metaTitle: Override the default meta title
+meta:
+  - name: description
+    content: hello
+  - name: keywords
+    content: super duper SEO
+# canonicalUrl: Set the canonical URL for the current page.
+---
+
+假文章 one
