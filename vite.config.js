98a5ed7f1450c0985ff672ee33a89222b09e1697
G100 <g100my.lan229@gmail.com>
2022-01-28 00:18:56 +0800

    init vite-plugin-pages setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import Components from 'unplugin-vue-components/vite'
+import Pages from 'vite-plugin-pages'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   // https://github.com/antfu/unplugin-vue-components
-  plugins: [Components()],
+  plugins: [Components(), Pages({ dirs: 'src/views' })],
 })
