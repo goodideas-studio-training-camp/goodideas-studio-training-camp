48b025a9646b1c1a9641ece7b52ca54e86d67af8
G100 <g100my.lan229@gmail.com>
2022-01-27 23:34:27 +0800

    init vite config(for dev), set unplugin-vue-components

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // https://github.com/antfu/unplugin-vue-components
+  plugins: [Components()],
+})
