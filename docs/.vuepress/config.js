450c8306ce6eb980a7d1001499feba3d570648de
G100 <g100my.lan229@gmail.com>
2022-01-29 23:35:34 +0800

    update vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,17 @@ const { path } = require('@vuepress/utils')
 const { makeNavRoute, makeSidebarRoute } = require('../../utils/routeMaker')
 const defineUserConfig = require('vuepress').defineUserConfig
 
+const folderNameMap = {
+  one: '第一個分類',
+  'demo-sub': 'demo 子目錄',
+}
+const exceptions = []
+
+const navs = makeNavRoute(
+  folderNameMap,
+  exceptions.concat(['index.md', '.vuepress'])
+)
+
 module.exports = defineUserConfig({
   base: '/Goodideas-studio-blog/',
   title: '好想工作室共同技術筆記',
@@ -10,18 +21,9 @@ module.exports = defineUserConfig({
   theme: path.resolve(__dirname, './theme'),
   themeConfig: {
     // https://v2.vuepress.vuejs.org/reference/default-theme/config.html#navbar
-    navbar: [
-      { text: 'Home', link: '/' },
-      ...makeNavRoute('one'),
-      ...makeNavRoute('two'),
-      ...makeNavRoute('demo'),
-    ],
+    navbar: [{ text: 'Home', link: '/' }, ...navs],
     // https://v2.vuepress.vuejs.org/reference/default-theme/config.html#sidebar
-    sidebar: {
-      ...makeSidebarRoute('one'),
-      ...makeSidebarRoute('two'),
-      ...makeSidebarRoute('demo'),
-    },
+    sidebar: [...navs],
   },
   bundlerConfig: {
     viteOptions: {
