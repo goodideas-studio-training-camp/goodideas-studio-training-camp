9aa381d08f655a25faaa0d4ea6220c8f6986ddb7
Cathy <shenzhu0831@gmail.com>
2022-10-03 15:57:55 +0800

    cathy 的第一篇文章

--- docs/.vuepress/authers/index.js
+++ docs/.vuepress/authers/index.js
@@ -2,4 +2,5 @@ import G100 from './G100.vue'
 import chris from './Chris.vue'
 import titangene from './Titangene.vue'
 import mangoSu from './mangoSu.vue'
-export { G100, chris, titangene, mangoSu }
+import Cathy from './Cathy/Cathy.vue'
+export { G100, chris, titangene, mangoSu, Cathy }
