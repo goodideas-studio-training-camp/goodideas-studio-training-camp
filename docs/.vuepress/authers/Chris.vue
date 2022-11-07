9dd0f659d7608393ed9765b84d46488780acd88e
chris <dwatow@gmail.com>
2022-11-07 16:37:23 +0800

    add search conosle file

--- docs/.vuepress/authers/Chris.vue
+++ docs/.vuepress/authers/Chris.vue
@@ -6,7 +6,7 @@
       <div><a href="https://dwatow.github.io/" target="_blank">《Chris 技術筆記》</a></div>
     </div>
     <ol>
-      鐵人文章
+      鐵人文章: 
       <li><a href="https://ithelp.ithome.com.tw/users/20107637/ironman/1457">前端新手村</a></li>
       <li><a href="https://ithelp.ithome.com.tw/users/20107637/ironman/1927">可不可以不要寫糙 code</a></li>
       <li><a href="https://ithelp.ithome.com.tw/users/20107637/ironman/3641">完美 Camp 進化論</a></li>
