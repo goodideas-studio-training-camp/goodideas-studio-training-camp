5792c2ac215cfa76693b47144c7729c059da4f4a
G100 <g100my.lan229@gmail.com>
2023-02-05 23:34:16 +0800

    style: delete useless file

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import Components from 'unplugin-vue-components/vite'
-import Pages from 'vite-plugin-pages'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  // https://github.com/antfu/unplugin-vue-components
-  plugins: [vue(), Components(), Pages({ dirs: 'src/components' })],
-})
